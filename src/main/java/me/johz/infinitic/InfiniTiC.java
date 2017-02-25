324b697877315995bfa4ee467d46bc894ce5256f
Lak Moore <stuartrickard@Stuarts-MBP.home>
2017-02-25 07:42:00 +0000

    Pre-New Laptop Backup

--- src/main/java/me/johz/infinitic/InfiniTiC.java
+++ src/main/java/me/johz/infinitic/InfiniTiC.java
@@ -34,7 +34,7 @@ public class InfiniTiC {
 	
 	public static final String NAME = "Infini-TiC";
     public static final String MODID = "infinitic";
-    public static final String VERSION = "1.7.10-0.1.4";
+    public static final String VERSION = "1.7.10-0.1.5";
     
     public static Logger LOGGER;
     public static File CONFIGDIR;
